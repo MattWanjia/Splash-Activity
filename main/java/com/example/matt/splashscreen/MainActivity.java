2259d9fc6dc6a3dccf5c868099abffbba8e21ab0
MattWanjia <wanjiamartin@gmail.com>
2019-06-27 08:46:31 +0300

    Add files via upload

---
+++ main/java/com/example/matt/splashscreen/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.matt.splashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.mainTV);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
